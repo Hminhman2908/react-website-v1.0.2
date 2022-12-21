04673f7b96c1480f826b6f6e52a8f9f8d15e6f3c
Huynh Minh Man <huynhman2908@gmail.com>
2022-12-21 15:28:11 +0700

    v1.0.2

---
+++ src/components/pages/Products.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+const Products = () => {
+    return (
+        <div className="products">PRODUCTS</div>
+    );
+};
+
+export default Products;
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+const SignUp = () => {
+    return (
+        <div className="sign-up">LIKE &amp; SUBSCRIBE</div>
+    );
+};
+
+export default SignUp;
