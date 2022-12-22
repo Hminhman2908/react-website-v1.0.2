2583bf400c92f4eb1416271f0436b981c3755b58
Huynh Minh Man <huynhman2908@gmail.com>
2022-12-22 10:43:27 +0700

    v1.0.2.8

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/react-website-v1.0.2">
+    {/* <Router basename="/react-website-v1.0.2"> */}
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
